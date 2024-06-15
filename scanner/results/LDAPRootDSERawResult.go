fe02b798f828c5da6b4ce2fbecda25bd3d6b0525
gustavoluvizotto <gustavoluvizotto@gmail.com>
2024-06-15 10:27:15 +0200

    seach with raw results (#2)
    use for fingerprinting of ldap servers

---
+++ scanner/results/LDAPRootDSERawResult.go
@@ -0,0 +1,43 @@
+package results
+
+import (
+	"encoding/base64"
+	"encoding/csv"
+	"github.com/rs/zerolog/log"
+	"github.com/tumi8/goscanner/scanner/misc"
+	"net"
+	"time"
+)
+
+type LDAPRootDSERawResult struct {
+	RawResponse []byte
+}
+
+func (t *LDAPRootDSERawResult) GetCsvFileName() string {
+	return FileLDAPRootDSERaw
+}
+
+func (t *LDAPRootDSERawResult) GetCsvHeader() []string {
+	return []string{
+		"id",
+		"ip",
+		"port",
+		"raw_response",
+	}
+}
+
+func (t *LDAPRootDSERawResult) WriteCsv(writer *csv.Writer, parentResult *ScanResult, synStart time.Time, synEnd time.Time, scanEnd time.Time, skipErrors bool, certCache *misc.CertCache) error {
+	ip, port, err := net.SplitHostPort(parentResult.Address)
+	if err != nil {
+		log.Err(err).Str("address", parentResult.Address).Msg("Could not split address into host and port parts.")
+	}
+
+	ldapRawResponse := base64.RawStdEncoding.EncodeToString(t.RawResponse)
+	return writer.Write([]string{
+		parentResult.Id.ToString(),
+		ip,
+		port,
+		ldapRawResponse,
+	})
+
+}
